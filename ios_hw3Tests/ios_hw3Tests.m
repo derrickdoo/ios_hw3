2b4a10758abd3358dcc0cf3bcda41640efdd4aba
derricko <derricko@yahoo-inc.com>
2014-06-20 14:58:15 -0700

    Initial Commit

---
+++ ios_hw3Tests/ios_hw3Tests.m
@@ -0,0 +1,34 @@
+//
+//  ios_hw3Tests.m
+//  ios_hw3Tests
+//
+//  Created by Derrick Or on 6/20/14.
+//  Copyright (c) 2014 derrickor. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ios_hw3Tests : XCTestCase
+
+@end
+
+@implementation ios_hw3Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
