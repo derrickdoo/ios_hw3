e74e7805413d27eebfb1e79349fc5ca932e2b07a
derricko <derricko@yahoo-inc.com>
2014-07-07 23:36:06 -0700

    forgot to add files for commit

--- MainViewController.m
+++ MainViewController.m
@@ -9,6 +9,12 @@
 #import "MainViewController.h"
 
 @interface MainViewController ()
+@property (nonatomic, assign) BOOL showMenu;
+
+@property (weak, nonatomic) IBOutlet UIImageView *carousel;
+@property (weak, nonatomic) IBOutlet UIScrollView *carouselScrollView;
+@property (weak, nonatomic) IBOutlet UIView *postContainer;
+- (IBAction)panPost:(UIPanGestureRecognizer *)sender;
 
 @end
 
@@ -26,7 +32,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+
+    self.showMenu = NO;
+    
+    //Define the size of the carouselScrollView's contents
+    CGSize sz = CGSizeMake(1428, 251);
+    self.carouselScrollView.contentSize = sz;
 }
 
 - (void)didReceiveMemoryWarning
@@ -35,4 +46,60 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)panPost:(UIPanGestureRecognizer *)sender {
+    //NSLog(@"test %d", self.showMenu);
+    
+    //pan stats
+    CGPoint translation = [sender translationInView:self.view];
+    //CGPoint point = [sender locationInView:self.view];
+    //CGPoint velocity = [sender velocityInView:self.view];
+    
+    //static vars
+    int maxYPos = self.view.frame.size.height - 80;
+    int transY = translation.y;
+    CGRect frame = self.postContainer.frame;
+    
+    //check to see if panning in progress
+    if (sender.state == UIGestureRecognizerStateChanged) {
+        //panning down
+        if(transY > 0) {
+            if (self.showMenu) {
+                frame.origin.y = maxYPos + (transY * 0.1);
+            }
+            else {
+                frame.origin.y = transY;
+            }
+        }
+        //panning up
+        else {
+            if(self.showMenu) {
+                frame.origin.y = maxYPos + transY;
+            }
+            else {
+                frame.origin.y = 0 + (transY * 0.1);
+            }
+        }
+    }
+    //check to see if the user finished panning
+    else if (sender.state == UIGestureRecognizerStateEnded) {
+        //NSLog(@"translation %@", NSStringFromCGPoint(translation));
+        
+        //panned down
+        if(transY > 0) {
+            self.showMenu = YES;
+            
+            frame.origin.y = maxYPos;
+        }
+        //panned up
+        else {
+            self.showMenu = NO;
+            
+            frame.origin.y = 0;
+        }
+    }
+
+    [UIView animateWithDuration:0.5 animations:^{
+        self.postContainer.frame = frame;
+    } completion:nil];
+}
 @end
--- ios_hw3/Main2ViewController.m
+++ ios_hw3/Main2ViewController.m
@@ -9,6 +9,9 @@
 #import "Main2ViewController.h"
 
 @interface Main2ViewController ()
+@property (weak, nonatomic) IBOutlet UIView *postView;
+@property (weak, nonatomic) IBOutlet UIScrollView *carouselScrollView;
+- (IBAction)postPan:(UIPanGestureRecognizer *)sender;
 
 @end
 
@@ -27,6 +30,9 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    //Define the size of the carouselScrollView's contents
+    CGSize sz = CGSizeMake(1404, 245);
+    self.carouselScrollView.contentSize = sz;
 }
 
 - (void)didReceiveMemoryWarning
@@ -34,5 +40,44 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
+- (IBAction)postPan:(UIPanGestureRecognizer *)sender {
+    //pan stats
+    CGPoint translation = [sender translationInView:self.view];
+    CGPoint point = [sender locationInView:self.view];
+    //CGPoint velocity = [sender velocityInView:self.view];
+    
+    NSLog(@"point info: %@", NSStringFromCGPoint(point));
+    
+    //static vars
+    int maxYPos = self.view.frame.size.height - 80;
+    int transY = translation.y;
+    CGRect frame = self.postView.frame;
+    
+    //check to see if panning in progress
+    if (sender.state == UIGestureRecognizerStateChanged) {
+        //panning down
+        if(transY > 0) {
+            frame.origin.y = transY;
+        }
+        //panning up
+        else {
+            frame.origin.y = maxYPos + transY;
+        }
+    }
+    //check to see if the user finished panning
+    else if (sender.state == UIGestureRecognizerStateEnded) {
+        //NSLog(@"translation %@", NSStringFromCGPoint(translation));
+        
+        //panned down
+        if(transY > 0) {
+            frame.origin.y = maxYPos;
+        }
+        //panned up
+        else {
+            frame.origin.y = 0;
+        }
+    }
+    
+    self.postView.frame = frame;
+}
 @end
